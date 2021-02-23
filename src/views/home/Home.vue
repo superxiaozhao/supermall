e83f025da881e778b36c5eead6f7d769100025e3
zhaojie <870362874@qq.com>
2021-02-23 13:10:17 +0800

    good

---
+++ src/views/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from 'network/home'
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[]
+
+      }
+    },
+    created(){
+      getHomeMultidata().then(res=>{
+        this.banners = res.data.banner
+        this.recommends = res.data.recommend
+      })
+    }
+  }
+</script>
+
+<style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+}
+</style>
